c253c9bdd9a9c10ede445b89a687416c9ae1d03a
EC2 Default User <ec2-user@ip-172-31-17-22.ap-southeast-1.compute.internal>
2019-05-30 12:29:44 +0000

    FutMatch2

--- db/migrate/20190515120126_add_column_application.rb
+++ db/migrate/20190515120126_add_column_application.rb
@@ -1,7 +1,7 @@
 class AddColumnApplication < ActiveRecord::Migration[5.0]
   def change
-    add_column :applications, :match_req_id, :integer
-    add_column :applications, :team_id, :integer
-    add_column :applications, :status, :string
+    # add_column :applications, :match_req_id, :integer
+    # add_column :applications, :team_id, :integer
+    # add_column :applications, :status, :string
   end
 end
--- db/migrate/20190515121249_drop_applications.rb
+++ db/migrate/20190515121249_drop_applications.rb
@@ -1,5 +1,5 @@
 class DropApplications < ActiveRecord::Migration[5.0]
   def change
-    drop_table :applications
+    # drop_table :applications
   end
 end
