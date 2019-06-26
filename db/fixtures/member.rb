d994e88aedfdb2683577db84485bfdf96118097d
EC2 Default User <ec2-user@ip-172-31-17-22.ap-southeast-1.compute.internal>
2019-06-26 04:18:30 +0000

    FutMatch

---
+++ db/fixtures/member.rb
@@ -0,0 +1,8 @@
+Team.seed(:id,
+  {:id => 2, :email => "t@g.com", :password => "111111", :name => "Team tajima", :rank => 3, :cat => "競技志向", :age_ave => 30},
+  {:id => 3, :email => "k@g.com", :password => "111111", :name => "Team kaneko", :rank => 5, :cat => "競技志向", :age_ave => 30},
+  {:id => 4, :email => "1@g.com", :password => "111111", :name => "Team1", :rank => 9, :cat => "都道府県", :age_ave => 40},
+  {:id => 5, :email => "2@g.com", :password => "111111", :name => "Team2", :rank => 3, :cat => "競技志向", :age_ave => 30},
+  {:id => 6, :email => "3@g.com", :password => "111111", :name => "Team3", :rank => 3, :cat => "競技志向", :age_ave => 30},
+  {:id => 7, :email => "4@g.com", :password => "111111", :name => "Team4", :rank => 3, :cat => "競技志向", :age_ave => 30}
+)
