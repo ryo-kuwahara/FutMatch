739abbb0feda39befb1c0cd8db0ed50c503beec9
EC2 Default User <ec2-user@ip-172-31-17-22.ap-southeast-1.compute.internal>
2019-07-05 10:47:52 +0000

    FutMatch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190616121728) do
+ActiveRecord::Schema.define(version: 20190705104652) do
 
   create_table "challenges", force: :cascade do |t|
     t.integer  "match_req_id"
